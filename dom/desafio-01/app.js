2a8183b95241f1c47525ba4226a07539f10784c6
Thiago Lino <thiagolinu@gmail.com>
2022-05-14 16:29:46 -0300

    aulas concluidas e desafio 1 feito

---
+++ dom/desafio-01/app.js
@@ -0,0 +1,16 @@
+new Vue({
+  el: "#desafio",
+
+  data: {
+    nome: "Thiago dos Santos Lino",
+    idade: 27,
+    linkImg:
+      "https://www.challengebasedlearning.org/wp-content/uploads/2019/02/code.jpg",
+  },
+
+  methods: {
+    randomNum() {
+      return Math.random();
+    },
+  },
+});
